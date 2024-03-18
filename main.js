d5224160fb179d32f48063e3d986fd0ba369c72c
Chamodi Nethmini <152781066+Chamodin@users.noreply.github.com>
2024-03-18 19:44:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".texts", {
+    strings: ["Backend Developer", "Frontend Developer", "Web Developer"],
+    typeSpeed: 100,
+    typeSpeed: 100,
+    typeSpeed: 100,
+    loop: true
+});
